0d329f4c811f2bb64df13c2af276a674e3991ee5
Komici <xbh719@126.com>
2021-06-02 19:12:49 +0800

    测试ResourceOwnerPassword授权模式

---
+++ ResourceOwnerPassword-IdentityServerDemo/Client/Controllers/ClientController.cs
@@ -0,0 +1,63 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    [Route("client")]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+            }
+
+            // request token
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "client_scope1",
+
+                UserName = "alice",
+                Password = "password",
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+
+            // call api
+            var client2 = new HttpClient();
+            client2.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client2.GetAsync("https://localhost:5001/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                //Console.WriteLine(JArray.Parse(content));
+            }
+
+            return Content(tokenResponse.Json.ToString());
+            //Console.WriteLine(tokenResponse.Json);
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
